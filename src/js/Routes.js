ffe4365c792f8ae89a8d49c8c404293a1e0c5ac5
Laurent.Wauters <lt.wauters@gmail.com>
2021-09-15 19:06:59 +0200

    adding db handler

--- src/js/Routes.js
+++ src/js/Routes.js
@@ -7,8 +7,13 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Overlay from 'react-bootstrap/Overlay';
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../img/logo-herwerkt-true.png";
 
 import { accounts } from './dbconn/mockAccounts';
+import { response } from 'express';
+
+import { initiateDBConnection } from './dbconn/DBHandler';
+
 //import InitiateConnection from './dbconn/DBHandler';
 //import GalleryTimeTable from './PDF/galleryTimeTable';
 
@@ -47,6 +52,7 @@ export default class Board extends React.Component {
             activeUser: {},
             languages: [],
             routes: [],
+            data: null,
         }
 
         this.renderList = this.renderList.bind(this);
@@ -66,7 +72,7 @@ export default class Board extends React.Component {
                 displayRoutes: !displayRoutes,
                 displayFile: !displayFile,
                 fileToDisplay: fileToDisplay,
-            })
+            });
         } else if (displayFile) {
             this.setState({
                 displayRoutes: !displayRoutes,
@@ -85,15 +91,30 @@ export default class Board extends React.Component {
         });
     }
 
+    changeOnlineState(user) {
+        //TODO write to DB or smt to change online state
+        if(user.activeState === 'online') {
+            user.activeState = 'offline';
+        } else if (user.activeState === 'offline') {
+            user.activeState = 'online';
+        }
+    }
+
     //checks if the date of purchase is still within the 31days time period
     checkIfValid(user) {
         const startDate = user.startDate;
         const now = Date.now();
         const dateDifference = now - startDate
+
         //check if more than 31 days (in milliseconds)
         if (dateDifference > 2678400000) {
             return false;
         }
+
+        if (user.activeState === 'online') {
+            return false;
+        }
+        this.changeOnlineState(user);
         return true;
     }
 
@@ -101,12 +122,12 @@ export default class Board extends React.Component {
     handleSubmit(event) {
         const loginscreen = this.state.loginscreen;
         const displayRoutes = this.state.displayRoutes;
-        const code = this.state.value;
+        const loginMail = this.state.value;
 
         const mockAccounts = accounts;
 
         mockAccounts.forEach((x) => {
-            if(x.id === code) {
+            if(x.email === loginMail) {
                 if(this.checkIfValid(x)) {
                     this.setState({
                         loginscreen: !loginscreen,
@@ -116,7 +137,7 @@ export default class Board extends React.Component {
                         routes: x.routes,
                     })
                 } else {
-                    alert("this code is not valid anymore");
+                    alert("this email is not valid anymore or is already in use.");
                 }
             } else {
                 console.log('error');
@@ -144,6 +165,33 @@ export default class Board extends React.Component {
 
     };
 
+    callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message);
+        }
+        return body
+    }
+
+    componentDidMount() {
+        this.callBackendAPI()
+            .then(res => this.setState({data: res.express}))
+            .catch(err => console.log(err));
+
+        window.addEventListener("beforeunload", this.unload);
+    }
+      
+    componentWillUnmount() {
+        window.removeEventListener("beforeunload", this.unload);
+    }
+      
+    unload = (e) => {
+        navigator.sendBeacon(`http://localhost:3000/window-closed/${this.props.username}`);
+        this.changeOnlineState(this.state.activeUser);
+    }
+
     renderList() {
         
         // const query = new URLSearchParams(window.location.search);
@@ -168,11 +216,11 @@ export default class Board extends React.Component {
             const distance = x.distance ?
             'Estimated distance: ' + x.distance :
             "no duration available";
-            const amountGalleries = x.amountGalleries ?
+            const amountGalleries = x.amountGalleries ?  
             'Amount of Galleries: ' + x.amountGalleries :
             'no galleries available';
             const image = x.image;
-
+  
             return (
                 <Col lg={4} md={4} sm={12} key={x.id}>
                     <Card value={x}className="text-black" onClick={this.handleClick.bind(this, x)} onMouseOver={() => this.handleOverlay()} onMouseLeave={() => this.handleOverlay()}>
@@ -192,18 +240,22 @@ export default class Board extends React.Component {
         if (this.state.loginscreen) {
             //Login Screen
             return (
-                <form>
-                    <h1>Enter code</h1>
-                    <input type="text" value={this.state.value} onChange={this.handleChange}></input>
-                    <button onClick={this.handleSubmit}>Submit</button>
-                </form>
+                <div class="wrapper login">
+                    <img src={logo} alt={"logo"}/>
+                    <form>
+                        <h1>Enter code</h1>
+                        <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+                        <button onClick={this.handleSubmit}>Submit</button>
+                    </form>
+                </div>
             )
         }
         else if (this.state.displayRoutes) {
             //Render of all the routes
             return (
-                <div className="wrapper">           
-                    <h1>Your routes:</h1>
+                <div className="wrapper">  
+                    <h1>Welcome {this.state.activeUser.firstName}</h1>         
+                    <h2>Your routes:</h2>
                     <Container>
                         <Row>
                             {routesMap}
--- src/js/dbconn/DBHandler.js
+++ src/js/dbconn/DBHandler.js
@@ -1,30 +1,42 @@
-import { Pouchdb, Find } from 'react-pouchdb'
-import { Suspense } from 'react'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useAsync } from 'react-async';
 
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 
-export default function InitiateConnection() {
+export function initiateDBConnection() {
+    const firebaseConfig = {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.PROJECT_ID + ".firebaseapp.com",
+        databaseURL: "https://" + process.env.PROJECT_ID + ".firebaseio.com",
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.PROJECT_ID + ".appspot.com",
+        messagingSenderId: process.env.SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+    };
 
-    return(
-        <Pouchdb name='dbname'>
-            <Suspense fallback='loading...'>
-                <Find 
-                    selector={{
-                        name: { $gte: null }
-                    }}
-                    sort={["name"]}
-                    children={({ db, docs }) => (
-                        <ul>
-                            {docs.map(doc => (
-                                <li key={doc.id}>
-                                    {doc.name}
-                                    <button onClick={() => db.remove(doc)}>Remove</button>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                />
-            </Suspense>
-        </Pouchdb>
-    )
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(firebaseApp);
+
+    onAuthStateChanged(auth, user => {
+        //check for user status
+    })
+
+    const db = getFirestore(app);
+
+    // Get list of accounts from DB
+    useEffect(() =>  {
+        try {
+            await function getAccounts(db) {
+                const accountsCol = collection(db, 'accounts');
+                const accountsSnapshot = await getDocs(accountsCol);
+                const accountList = accountsSnapshot.docs.map(doc => doc.data());
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    })
+
 }
-
